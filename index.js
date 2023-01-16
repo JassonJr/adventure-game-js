dc9efede4c0adc328f0b35407819b0d72e190b1c
jassonjr <jassonjrsdm@gmail.com>
2023-01-16 17:16:46 -0300

    physics and player  movements added

--- index.js
+++ index.js
@@ -4,5 +4,31 @@ const c = canvas.getContext('2d')
 canvas.width = 1024
 canvas.height = 576
 
-c.fillStyle = 'gray'
-c.fillRect(0, 0, canvas.width, canvas.height)
+const player = new Player()
+
+const keys = {
+    w: {
+        pressed: false
+    },
+    a: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    }
+}
+
+function animate() {
+    window.requestAnimationFrame(animate)
+    c.fillStyle = 'gray'
+    c.fillRect(0, 0, canvas.width, canvas.height)
+
+    player.velocity.x = 0
+    if (keys.d.pressed) player.velocity.x = 5
+    else if (keys.a.pressed) player.velocity.x = -5
+
+    player.draw()
+    player.update()
+}
+
+animate()
